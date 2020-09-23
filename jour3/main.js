ec5322ef4b358c72f58c05f6ba558da4e78a4f86
florian13100 <florian.gobin@3wa.io>
2020-09-23 15:18:20 +0200

    exo 3

---
+++ jour3/main.js
@@ -0,0 +1,2 @@
+import {add as add} from "math.js";
+import {sub as sub} from "math.js";
---
+++ jour3/math.js
@@ -0,0 +1,15 @@
+export const add = (a, b) => {
+    return console.log(a + b);
+  }
+
+export const sub = (a, b) => {
+    return console.log(a - b);
+  }
+
+export const mult = (a, b) => {
+    return console.log(a * b);
+  }
+
+export const div = (a, b) => {
+    return console.log(a / b);
+  }
